3459c9812afe94f1a0991cf1cde6f9d016757127
lavie <895530239@qq.com>
2021-01-11 22:27:17 +0800

    初步搭建

--- index.js
+++ index.js
@@ -1,16 +1,68 @@
 #!/usr/bin/env node
-
-const { program } = require('commander')
+// --这种用法是为了防止操作系统用户没有将node装在默认的/usr/bin路径里。当系统看到这一行的时候，
+// 首先会到env设置里查找node的安装路径，再调用对应路径下的解释器程序完成操作。
+const program = require("commander");
+const download = require("download-git-repo");
+const inquirer = require("inquirer");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const ora = require("ora");
+const chalk = require("chalk");
+const symbols = require("log-symbols");
 
 program
-    .version('0.1.0')  // --version 版本
-    .command('init <name> [branch]') // 初始化命令
-    .description('初始化项目文件')
-    .action( (name,branch) => { // 得到name 
-        console.log('Hello World') // 进行输出
-    })
-
-program.parse(process.argv) // 解析变量
-
-
+  .version("0.0.1", "-v, --version")
+  .command("init <name>")
+  //program滴回调
+  .action(name => {
+    if (fs.existsSync(name)) {
+      // 错误提示项目已存在，避免覆盖原有项目
+      console.log(symbols.error, chalk.red("项目已存在"));
+      return;
+    }
+    inquirer
+      .prompt([
+        {
+          name: "description",
+          message: "请输入项目描述"
+        },
+        {
+          name: "author",
+          message: "请输入作者名称"
+        }
+      ])
+      .then(answers => {
+        /* 坑：'git clone' failed with status 128 通过git clone拉取出错
+         将git改为通过 direct: http:// URl的方式进行clone */
+        download(
+          "direct:https://github.com/Lavieenrose99/ivanCli.git#master",
+          name,
+          { clone: true },
+          err => {
+            const spinner = ora("正在下载模板...");
+            spinner.start();
+            if (!err) {
+              spinner.succeed();
+              const meta = {
+                name,
+                description: answers.description,
+                author: answers.author
+              };
+              const fileName = `${name}/package.json`;
+              if (fs.existsSync(fileName)) {
+                const content = fs.readFileSync(fileName).toString();
+                const result = handlebars.compile(content)(meta);
+                fs.writeFileSync(fileName, result);
+              }
+              console.log(symbols.success, chalk.green("项目初始化完成"));
+            } else {
+              spinner.fail();
+              console.log(symbols.error, chalk.red(`拉取远程仓库失败${err}`));
+            }
+          }
+        );
+      });
+  });
+//解析命令行
+program.parse(process.argv);
 
